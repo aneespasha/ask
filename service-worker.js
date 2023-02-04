65c835564a9f96f56092a749ef7eb7534a1aeb73
Anees Pasha Sk <aneespasha@gmail.com>
2023-02-04 10:45:03 +0530

    feat[ask-responsive]:responsive amends

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.747f7d70b0d85ff6f0ca533af70187a3.js"
+  "/precache-manifest.98fdacd390c0175b837c851b858c83ae.js"
 );
 
 self.addEventListener('message', (event) => {
